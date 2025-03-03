39bd2cbf81f7947bbc4c19fd2f5af1f72bc2a187
Kristiāns <11DKKokars@rvt.lv>
2025-03-03 18:54:29 +0200

    initial

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        Log::info('Validating rules:', $this->all());
+        return [
+            'name' => 'required|string|max:255',
+            'nickname' => 'required|string|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'password_confirmation' => 'required|same:password',
+        ];
+    }
+
+    protected function failedValidation(\Illuminate\Contracts\Validation\Validator $validator)
+    {
+        Log::error('Validation failed:', $validator->errors()->toArray());
+        parent::failedValidation($validator);
+    }
+}
