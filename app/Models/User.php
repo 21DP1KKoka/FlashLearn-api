0ddde63cbcfb5f353f7e55f656cffe2439bb7b67
21DP1KKoka <11DKKokars@rvt.lv>
2025-05-26 05:33:00 +0300

    api progressv2

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,8 +21,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'nickname',
-        'name',
+        'username',
         'email',
         'password',
     ];
